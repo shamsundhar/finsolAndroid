9ecf9835cf6342e8219cc3feb3f143bf2bc506fe
syammanu <shyam.kdp@gmail.com>
2022-04-03 02:09:22 +0530

    project skeleton prepared

---
+++ app/src/main/java/com/finsol/tech/api/ClearScoreApiService.kt
@@ -0,0 +1,12 @@
+package com.finsol.tech.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ClearScoreApiService {
+
+//        @GET("/endpoint.json")
+//        suspend fun getCreditScore() : Score
+
+}
---
+++ app/src/main/java/com/finsol/tech/api/NoConnectivityException.kt
@@ -0,0 +1,10 @@
+package com.finsol.tech.api
+
+import java.io.IOException
+
+class NoConnectivityException : IOException() {
+        // You can send any message whatever you want from here.
+        override val message: String
+            get() = "No Internet Connection"
+        // You can send any message whatever you want from here.
+    }
