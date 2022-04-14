3638005ec142f9781d3590059f33fd7126062c9d
syammanu <shyam.kdp@gmail.com>
2022-04-14 10:56:54 +0530

    funds screen completed

--- app/src/main/java/com/finsol/tech/presentation/account/AccountChangePasswordFragment.kt
+++ app/src/main/java/com/finsol/tech/presentation/account/AccountChangePasswordFragment.kt
@@ -6,11 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.finsol.tech.R
-import com.finsol.tech.databinding.FragmentAccountSettingsBinding
+import com.finsol.tech.databinding.FragmentAccountChangePasswordBinding
 
-class AccountSettingsFragment: Fragment() {
-    private lateinit var binding: FragmentAccountSettingsBinding
+class AccountChangePasswordFragment: Fragment() {
+    private lateinit var binding: FragmentAccountChangePasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,7 +19,7 @@ class AccountSettingsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAccountSettingsBinding.inflate(inflater, container, false)
+        binding = FragmentAccountChangePasswordBinding.inflate(inflater, container, false)
         binding.toolbar.backButton.visibility = View.VISIBLE
         binding.toolbar.subTitle.visibility = View.GONE
         binding.toolbar.title.visibility = View.GONE
@@ -28,8 +27,8 @@ class AccountSettingsFragment: Fragment() {
         binding.toolbar.backButton.setOnClickListener {
             activity?.onBackPressed()
         }
-        binding.changePasswordLayout.setOnClickListener {
-            findNavController().navigate(R.id.accountChangePasswordFragment)
+        binding.changePasswordButton.setOnClickListener {
+            findNavController().popBackStack()
         }
         return binding.root
     }
