cdceae6721116dac3c8eb8fe61f8a6f7a6d8b9ce
syammanu <shyam.kdp@gmail.com>
2022-04-25 18:49:16 +0530

    icons

--- app/src/main/java/com/finsol/tech/presentation/portfolio/PortfolioFragment.kt
+++ app/src/main/java/com/finsol/tech/presentation/portfolio/PortfolioFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.finsol.tech.R
+import com.finsol.tech.databinding.FragmentPortfolioBinding
 import com.finsol.tech.presentation.base.BaseFragment
+import com.finsol.tech.presentation.orders.adapter.OrdersPendingAdapter
+import com.finsol.tech.presentation.portfolio.adapter.PortfolioAdapter
+import com.finsol.tech.presentation.watchlist.WatchListModel
 
 class PortfolioFragment: BaseFragment(){
+    private lateinit var binding: FragmentPortfolioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,7 +25,35 @@ class PortfolioFragment: BaseFragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentPortfolioBinding.inflate(inflater, container, false)
+        binding.toolbar.title.visibility = View.VISIBLE
+        binding.toolbar.subTitle.visibility = View.VISIBLE
+        binding.toolbar.profilePic.visibility = View.VISIBLE
+        // this creates a vertical layout Manager
+        binding.portfolioRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        return inflater.inflate(R.layout.fragment_portfolio, container, false)
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<WatchListModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(WatchListModel("Name"+i, "price " + i, "time"+i, "city"+i, "value"+i))
+        }
+        // This will pass the ArrayList to our Adapter
+        val adapter = PortfolioAdapter(data)
+        adapter.setOnItemClickListener(object: PortfolioAdapter.ClickListener {
+            override fun onItemClick(model: WatchListModel) {
+//                val bundle = Bundle()
+//                bundle.putParcelable("selectedModel", model)
+//                findNavController().navigate(R.id.orderPendingDetailsFragment, bundle)
+            }
+        })
+
+        // Setting the Adapter with the recyclerview
+        binding.portfolioRecyclerView.adapter = adapter
+
+
+        return binding.root
     }
 }
