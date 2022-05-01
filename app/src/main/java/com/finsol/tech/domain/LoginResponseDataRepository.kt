f71f75f2aeb08c77cdb6a85c43d07dbb75f768d9
phanindra <phanidevlancer@gmail.com>
2022-05-01 12:00:44 +0530

    Portfolio api changes

--- app/src/main/java/com/finsol/tech/domain/LoginResponseDataRepository.kt
+++ app/src/main/java/com/finsol/tech/domain/LoginResponseDataRepository.kt
@@ -7,6 +7,6 @@ interface LoginResponseDataRepository {
     suspend fun getLoginData(userID: String, password: String): Flow<ResponseWrapper<LoginResponse>>
     suspend fun getProfileData(userID: String): Flow<ResponseWrapper<ProfileResponse>>
     suspend fun getAllContractsData(userID: String): Flow<ResponseWrapper<GetAllContractsResponse>>
-    suspend fun getPendingOrdersData(userID: String): Flow<ResponseWrapper<PendingOrderResponse>>
+    suspend fun getPendingOrdersData(userID: String): Flow<ResponseWrapper<Array<PendingOrderModel>>>
     suspend fun getPortfolioResponse(userID: String): Flow<ResponseWrapper<PortfolioResponse>>
 }
